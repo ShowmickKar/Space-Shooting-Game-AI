d84f10d05121c4b7074be8be06a3ede95a06e1a0
Showmick Kar <showmick12@gmail.com>
2021-02-06 01:17:11 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import pygame
+import os
+from spaceship import Spaceship
+from pygame import mixer
+
+pygame.init()
+pygame.font.init()
+mixer.init()
+myfont = pygame.font.SysFont("Comic Sans MS", 25)
+big_font = pygame.font.SysFont("Comic Sans MS", 70)
+
+BACKGROUND_MUSIC = mixer.Sound("Assets/04.JUMP IN THE FIRE.wav")
+BACKGROUND_MUSIC.play(-1)
+
+WIDTH, HEIGHT = 1000, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooting Game")
+
+BACKGROUND = pygame.transform.scale(
+    pygame.image.load("./Assets/Background.png"), (WIDTH, HEIGHT)
+)
+
+BORDER = ((WIDTH // 2) - 5, 0, 10, HEIGHT)
+SPACESHIP_SIZE = 65
+
+spaceship_1 = pygame.transform.rotate(
+    pygame.transform.scale(
+        pygame.image.load("Assets/spaceship-1.png"), (SPACESHIP_SIZE, SPACESHIP_SIZE)
+    ),
+    270,
+)
+
+spaceship_2 = pygame.transform.rotate(
+    pygame.transform.scale(
+        pygame.image.load("Assets/spaceship-2.png"), (SPACESHIP_SIZE, SPACESHIP_SIZE)
+    ),
+    90,
+)
+
+BULLET_SIZE = 50, 40
+bullet_1_img = pygame.transform.rotate(
+    pygame.transform.scale(pygame.image.load("Assets/Bullet-1.png"), BULLET_SIZE),
+    270,
+)
+bullet_2_img = pygame.transform.rotate(
+    pygame.transform.scale(pygame.image.load("Assets/Bullet-2.png"), BULLET_SIZE),
+    90,
+)
+
+FPS = 60
+INITIAL_HEALTH = 10
+VELOCITY = 7
+MAX_BULLETS = 3
+BULLET_VELOCITY = 15
+
+
+def draw(window, left_spaceship, right_spaceship):
+    window.blit(BACKGROUND, (0, 0))
+    pygame.draw.rect(window, (0, 0, 0), BORDER)
+    health_player_1 = myfont.render(
+        f"HEALTH: {left_spaceship.getHealth()}", False, (255, 255, 255)
+    )
+    health_player_2 = myfont.render(
+        f"HEALTH: {right_spaceship.getHealth()}", False, (255, 255, 255)
+    )
+    window.blit(health_player_1, (0, 0))
+    window.blit(health_player_2, (BORDER[0] + 10, 0))
+    left_spaceship.draw(window)
+    right_spaceship.draw(window)
+    pygame.display.update()
+
+
+def reset(window, winner):
+    pygame.time.delay(500)
+    game_over = big_font.render("WINS!", False, (255, 255, 0))
+    pygame.display.update()
+    if winner == 1:
+        window.blit(game_over, (WIDTH // 2 - 320, HEIGHT // 2 - 50))
+    else:
+        window.blit(game_over, (WIDTH - (WIDTH // 2 - 180), HEIGHT // 2 - 50))
+    pygame.display.update()
+    pygame.time.delay(1000)
+    main(window)
+
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+    left_spaceship = Spaceship(
+        [100, 100],
+        SPACESHIP_SIZE,
+        "left",
+        INITIAL_HEALTH,
+        VELOCITY,
+        MAX_BULLETS,
+        BULLET_SIZE,
+        bullet_1_img,
+        BULLET_VELOCITY,
+        spaceship_1,
+        WIDTH,
+        HEIGHT,
+        BORDER,
+    )
+    right_spaceship = Spaceship(
+        [WIDTH - 100, HEIGHT - 100],
+        SPACESHIP_SIZE,
+        "right",
+        INITIAL_HEALTH,
+        VELOCITY,
+        MAX_BULLETS,
+        BULLET_SIZE,
+        bullet_2_img,
+        BULLET_VELOCITY,
+        spaceship_2,
+        WIDTH,
+        HEIGHT,
+        BORDER,
+    )
+
+    while run:
+        clock.tick(FPS)
+        draw(window, left_spaceship, right_spaceship)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL:
+                    left_spaceship.shoot()
+                # if event.key == pygame.K_RCTRL:
+                #     right_spaceship.shoot()
+        navigation_key = pygame.key.get_pressed()
+        if navigation_key[pygame.K_w]:
+            left_spaceship.moveUp()
+        if navigation_key[pygame.K_s]:
+            left_spaceship.moveDown()
+        if navigation_key[pygame.K_d]:
+            left_spaceship.moveRight()
+        if navigation_key[pygame.K_a]:
+            left_spaceship.moveLeft()
+
+        # if navigation_key[pygame.K_UP]:
+        #     right_spaceship.moveUp()
+        # if navigation_key[pygame.K_DOWN]:
+        #     right_spaceship.moveDown()
+        # if navigation_key[pygame.K_RIGHT]:
+        #     right_spaceship.moveRight()
+        # if navigation_key[pygame.K_LEFT]:
+        #     right_spaceship.moveLeft()
+
+        """
+            Program the AI for the right side player
+        """
+
+        # Calling the AI method for the right spaceship
+        right_spaceship.AI(left_spaceship)
+
+        if left_spaceship.collision(right_spaceship):
+            pygame.mixer.music.load("Assets/Collision.wav")
+            pygame.mixer.music.play()
+            left_spaceship.healthDecrease()
+        if right_spaceship.collision(left_spaceship):
+            pygame.mixer.music.load("Assets/Collision.wav")
+            pygame.mixer.music.play()
+            right_spaceship.healthDecrease()
+        if left_spaceship.getHealth() <= 0:
+            reset(window, 2)
+        if right_spaceship.getHealth() <= 0:
+            reset(window, 1)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main(window)
---
+++ spaceship.py
@@ -0,0 +1,161 @@
+import pygame
+from bullet import Bullet
+
+pygame.mixer.init()
+
+
+class Spaceship:
+    def __init__(
+        self,
+        position,
+        size,
+        placement,
+        initial_health,
+        velocity,
+        max_bullets,
+        bullet_size,
+        bullet_img,
+        bullet_velocity,
+        img,
+        WIDTH,
+        HEIGHT,
+        border,
+    ):
+
+        self.__size = size
+        self.__placement = placement
+        self.__img = img
+        self.__health = initial_health
+        self.__velocity = velocity
+        self.__position = pygame.Rect(position[0], position[1], size, size)
+        self.__border = border
+        self.__max_bullets = max_bullets
+        self.bullets = []
+        self.__bullet_size = bullet_size
+        self.__bullet_img = bullet_img
+        self.__bullet_velocity = bullet_velocity
+        self.window_width = WIDTH
+        self.window_height = HEIGHT
+        self.__flag = 1  # for the AI feature
+
+    def moveUp(self):
+        if self.__position.y - self.__velocity >= 0:
+            self.__position.y -= self.__velocity
+
+    def moveDown(self):
+        if self.__position.y + self.__velocity + self.__size <= self.window_height:
+            self.__position.y += self.__velocity
+
+    def moveRight(self):
+        if self.__placement == "right":
+            if self.__position.x + self.__velocity + self.__size <= self.window_width:
+                self.__position.x += self.__velocity
+        else:
+            if self.__position.x + self.__velocity + self.__size <= self.__border[0]:
+                self.__position.x += self.__velocity
+
+    def moveLeft(self):
+        if self.__placement == "right":
+            if (
+                self.__position.x - self.__velocity
+                >= self.__border[0] + self.__border[2]
+            ):
+                self.__position.x -= self.__velocity
+        else:
+            if self.__position.x - self.__velocity >= 0:
+                self.__position.x -= self.__velocity
+
+    def shoot(self):
+        if len(self.bullets) < self.__max_bullets:
+            pygame.mixer.music.load("Assets/Gun-Fire.wav")
+            pygame.mixer.music.play()
+            if self.__placement == "right":
+                self.bullets.append(
+                    Bullet(
+                        [
+                            self.__position.x,
+                            self.__position.y
+                            + self.__size // 2
+                            - self.__bullet_size[0] // 2,
+                        ],
+                        self.__bullet_size,
+                        -1,
+                        self.__bullet_velocity,
+                        self.__bullet_img,
+                        self.window_width,
+                        self.window_height,
+                    )
+                )
+            else:
+                self.bullets.append(
+                    Bullet(
+                        [
+                            self.__position.x + self.__bullet_size[1],
+                            self.__position.y
+                            + self.__size // 2
+                            - self.__bullet_size[0] // 2,
+                        ],
+                        self.__bullet_size,
+                        1,
+                        self.__bullet_velocity,
+                        self.__bullet_img,
+                        self.window_width,
+                        self.window_height,
+                    )
+                )
+
+    def getPosition(self):
+        return self.__position
+
+    def getHealth(self):
+        return self.__health
+
+    def healthDecrease(self):
+        self.__health -= 1
+
+    def collision(self, opponent):
+        for bullet in opponent.bullets:
+            if self.__position.colliderect(bullet.getPosition()):
+                opponent.bullets.remove(bullet)
+                return True
+
+    def draw(self, window):
+        window.blit(self.__img, (self.__position.x, self.__position.y))
+        for bullet in self.bullets:
+            flag = bullet.move()
+            if flag == False:
+                self.bullets.remove(bullet)
+            bullet.draw(window)
+
+    ## implement the AI feature
+    ## for the right sided player
+    def AI(self, opponent):
+        for bullet in opponent.bullets:
+            if (
+                bullet.getPosition()[1] + bullet.getSize()[1] >= self.__position.y
+                and bullet.getPosition()[1]
+                <= self.__position.y + self.__size + self.__velocity
+            ):
+                if self.__flag:
+                    if self.__position.y - self.__velocity >= 0:
+                        self.moveUp()
+                    else:
+                        self.__flag = 0
+                else:
+                    if (
+                        self.__position.y + self.__velocity + self.__size
+                        <= self.window_height
+                    ):
+                        self.moveDown()
+                    else:
+                        self.__flag = 1
+        if abs(self.__position.y - opponent.getPosition().y) <= 10:
+            dist = True
+            for bullet in self.bullets:
+                if abs(
+                    bullet.getPosition().y - (self.__position.y + self.__size // 2)
+                    <= 20
+                ):
+                    dist = False
+            if dist:
+                self.shoot()
